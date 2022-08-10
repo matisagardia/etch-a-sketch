604807715491ae069f6720abe35c6415f847dd7c
Matias Sagardia <94998926+matisagardia@users.noreply.github.com>
2022-08-10 12:09:19 -0300

    Eraser and clear done

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-//Variables declatarion
-
-
-let color = document.querySelector('color');
-let rainbow = document.querySelector('rainbow');
-let eraser = document.querySelector('eraser');
-let clear = document.querySelector('clear');
-let option1 = document.querySelector('option1');
-let option2 = document.querySelector('option2');
-let option3 = document.querySelector('option3');
-let option4 = document.querySelector('option4');
-
-
-// Create grid
+// Create grid and hovering for color change
 
 function createGrid(size){
 
@@ -24,19 +11,62 @@ for (let i = 0; i < size * size; i++) {
   let square = document.createElement('div');
   etchScreen.appendChild(square).className = 'square';
 }
+
+let squares = document.querySelectorAll('.square');
+squares.forEach(square => {
+  square.style.backgroundColor = 'white';
 }
-createGrid(16);
+);
 
-// Hovering for color change
+squares.forEach(square => 
+  {square.addEventListener('mouseover', (e) => 
+    {square.style.backgroundColor = 'black';});
+});
+}
 
-function colorChange(){
+// Different grid sizes
+
+let option1 = document.querySelector('.option1');
+option1.addEventListener('click', (e) => {(createGrid(15))});
+let option2 = document.querySelector('.option2');
+option2.addEventListener('click', (e) => {(createGrid(30))});
+let option3 = document.querySelector('.option3');
+option3.addEventListener('click', (e) => {(createGrid(45))});
+let option4 = document.querySelector('.option4');
+option4.addEventListener('click', (e) => {(createGrid(60))});
+
+//Clear the grid
+
+function clearGrid() {
+let clear = document.querySelector('.clear');
+clear.addEventListener('click', (e) => {
   let squares = document.querySelectorAll('.square');
-  squares.forEach(square => {square.addEventListener('mouseover', (e) => {square.style.backgroundColor = 'black';});
-  });
-} 
-colorChange();
+  squares.forEach(square => {
+    square.style.backgroundColor = 'white';
+  }
+  );
+})
+}
 
-  
+clearGrid();
+
+function eraser() {
+  let eraser = document.querySelector('.eraser');
+  eraser.addEventListener('click', (e) => {
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+      square.addEventListener('mouseover', (e) => {
+        square.style.backgroundColor = 'white';})
+    })}
+    );
+  }
+
+  eraser();
+
+
+
+
+
     
 
 
